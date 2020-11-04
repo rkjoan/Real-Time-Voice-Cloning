ebe6ee8cc74498792575f493b34f2c2e150b3f51
Roland Joan <lukegrasseofficial@gmail.com>
2020-11-03 21:47:34 -0800

    Update hparams.py

--- synthesizer/hparams.py
+++ synthesizer/hparams.py
@@ -340,7 +340,7 @@ hparams = HParams(
     
     
     ### SV2TTS ###
-    speaker_embedding_size=256,
+    speaker_embedding_size=768,
     silence_min_duration_split=0.4, # Duration in seconds of a silence for an utterance to be split
     utterance_min_duration=1.6,     # Duration in seconds below which utterances are discarded
     
